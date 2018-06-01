9ce01aa653d84dea121f772f2883230646c736b3
hckang <hckang80@gmail.com>
2018-06-01 23:56:54 +0900

    스토어 생성 및 연결

--- src/index.js
+++ src/index.js
@@ -4,5 +4,16 @@ import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 registerServiceWorker();
